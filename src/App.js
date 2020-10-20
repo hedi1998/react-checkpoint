bb97d3a929a83f9305a5cb59adcd0d6acb7e389b
hedi1998 <magrounhedi8@gmail.com>
2020-10-20 21:11:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './form'
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
